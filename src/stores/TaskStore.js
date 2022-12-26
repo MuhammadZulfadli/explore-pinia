f172809a54f6b55cfd48a5d117eb6bc2f01371d8
zulfadli <zulfadlisimatupang@ecampus.ut.ac.id>
2022-12-26 14:59:50 +0700

    init project

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,23 @@
+/**
+ * Store
+ */
+
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [
+      {
+        id: 1,
+        title: "Buy some egg",
+        isFav: false,
+      },
+      {
+        id: 2,
+        title: "Buy some Keyboard",
+        isFav: true,
+      },
+    ],
+    name: "Ipay",
+  }),
+});
