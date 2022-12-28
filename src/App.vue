94f2979dfa765543996eeb0a31f1d62f1ea3176f
zulfadli <zulfadlisimatupang@ecampus.ut.ac.id>
2022-12-28 13:25:38 +0700

    learn: asynchronous in actions

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@
       <TaskForm />
     </div>
 
+    <div class="loading" v-if="loading">Loading...</div>
+
     <!-- Filter -->
     <nav class="filter">
       <button @click="filter = 'all'">All Taks</button>
@@ -19,16 +21,19 @@
 
     <!-- Task Lists -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} fav left to do</p>
-      <div v-for="task in taskStore.favs" :key="task">
+      <p>You have {{ favCount }} fav left to do</p>
+      <div v-for="task in favs" :key="task">
         <TaskDetails :task="task" />
       </div>
+    </div>
+    <div class="reset">
+      <button @click="taskStore.$reset">Reset State</button>
     </div>
   </main>
 </template>
@@ -38,6 +43,7 @@ import { ref } from "vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import TaskForm from "./components/TaskForm.vue";
 import { useTaskStore } from "./stores/TaskStore";
+import { storeToRefs } from "pinia";
 export default {
   components: {
     TaskDetails,
@@ -50,7 +56,15 @@ export default {
     const taskStore = useTaskStore();
     const filter = ref("all");
 
-    return { taskStore, filter };
+    const { tasks, loading, favs, totalCount, favCount } =
+      storeToRefs(taskStore);
+
+    /**
+     * Fetch
+     */
+    taskStore.getTasks();
+
+    return { taskStore, filter, tasks, loading, favs, totalCount, favCount };
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,18 +6,8 @@ import { defineStore } from "pinia";
 
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
-    tasks: [
-      {
-        id: 1,
-        title: "Buy some egg",
-        isFav: false,
-      },
-      {
-        id: 2,
-        title: "Buy some Keyboard",
-        isFav: true,
-      },
-    ],
+    tasks: [],
+    loading: false,
   }),
   /**
    *Getters
@@ -53,17 +43,53 @@ export const useTaskStore = defineStore("taskStore", {
    * Actions
    */
   actions: {
-    addTask(task) {
-      this.tasks.push(task);
+    async getTasks() {
+      this.loading = true;
+      try {
+        const response = await fetch("http://localhost:3000/tasks");
+        const data = await response.json();
+        this.tasks = data;
+      } catch (error) {
+        console.log(error);
+      }
+      this.loading = false;
     },
-    deleteTask(id) {
+    async addTask(task) {
+      this.tasks.push(task);
+
+      const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.error) {
+        console.log(response.error);
+      }
+    },
+    async deleteTask(id) {
       this.tasks = this.tasks.filter((t) => {
         return t.id !== id;
       });
+
+      const response = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "DELETE",
+      });
+      if (response.error) {
+        console.log(response.error);
+      }
     },
-    toggleFav(id) {
+    async toggleFav(id) {
       const task = this.tasks.find((t) => t.id == id);
       task.isFav = !task.isFav;
+
+      const response = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "PATCH",
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.error) {
+        console.log(response.error);
+      }
     },
   },
 });
